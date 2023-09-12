cf1d61014c27d85e4fa6cae1d1ea1083567aae60
mint <ge10rik@gmail.com>
2023-09-12 23:21:42 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <>
+      <Button type="primary">Button</Button>
+      <div>tst font</div>
+    </>
+  );
+}
+
+export default App;
